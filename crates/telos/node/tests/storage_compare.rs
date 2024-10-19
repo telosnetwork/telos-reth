098efd5e1630116c78790c22dbe10c9cefcb9bd0
Jesse Schulman <jesse@caleos.io>
2024-10-19 15:17:32 -0700

    Adding the start of a storage compare script

---
+++ crates/telos/node/tests/storage_compare.rs
@@ -0,0 +1,58 @@
+use alloy_provider::ProviderBuilder;
+use antelope::api::client::{APIClient, DefaultProvider};
+use antelope::api::v1::structs::{GetTableRowsParams, TableIndexType};
+use antelope::chain::name::Name;
+use antelope::name;
+use reqwest::Url;
+use telos_translator_rs::types::evm_types::AccountRow;
+
+// #[tokio::test]
+pub async fn compare() {
+    let evm_rpc = "http://localhost:8545";
+    // let telos_rpc = "http://192.168.0.20:8884";
+    let telos_rpc = "http://38.91.106.49:9000";
+    let block_delta = 57;
+
+    let api_client = APIClient::<DefaultProvider>::default_provider(telos_rpc.into(), Some(5)).unwrap();
+    let info = api_client.v1_chain.get_info().await.unwrap();
+
+
+    // let provider = ProviderBuilder::new()
+    //     .on_http(Url::from_str(evm_rpc).unwrap());
+
+    println!("Telos chain info: {:?}", info);
+    let evm_block_num = info.head_block_num - block_delta;
+
+    let mut has_more = true;
+    let mut lower_bound = Some(TableIndexType::UINT64(0));
+
+    let mut count = 0;
+
+    while has_more {
+        let mut query_params = GetTableRowsParams {
+            code: name!("eosio.evm"),
+            table: name!("account"),
+            scope: None,
+            lower_bound,
+            upper_bound: None,
+            limit: Some(5000),
+            reverse: None,
+            index_position: None,
+            show_payer: None,
+        };
+        let account_rows = api_client.v1_chain.get_table_rows::<AccountRow>(query_params).await;
+        if let Ok(account_rows) = account_rows {
+            lower_bound = account_rows.next_key;
+            has_more = lower_bound.is_some();
+            for account_row in account_rows.rows {
+                println!("Account: {:?}", account_row.address.as_string());
+                count += 1;
+                lower_bound = Some(TableIndexType::UINT64(account_row.index + 1));
+            }
+        } else {
+            panic!("Failed to fetch account row");
+        }
+    }
+
+    println!("Total account rows: {}", count);
+}
