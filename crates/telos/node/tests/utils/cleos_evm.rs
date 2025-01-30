c5be3d5e59f5c8ba3eb2b21ab94a503dbd706210
Guillermo Rodriguez <guillermo@telos.net>
2025-01-30 15:05:54 -0300

    Fix wrong authorization account on transfer_tx helper

--- crates/telos/node/tests/utils/cleos_evm.rs
+++ crates/telos/node/tests/utils/cleos_evm.rs
@@ -91,7 +91,7 @@ pub fn transfer_tx(
     let rev_act = Action::new_ex(
         name!("eosio.token"),
         name!("transfer"),
-        vec![PermissionLevel::new(name!("eosio.evm"), name!("active"))],
+        vec![PermissionLevel::new(from, name!("active"))],
         raw_data,
     );
 
