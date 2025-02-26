0d97c62a5687fb0ecb0948ed9914a08a5902b3ea
Amir Pasha Motamed <pasha@telos.net>
2025-02-26 16:11:23 +0330

    Add setrevision sandwich

--- crates/telos/node/tests/utils/cleos_evm.rs
+++ crates/telos/node/tests/utils/cleos_evm.rs
@@ -1,10 +1,11 @@
 use std::str::FromStr;
 use alloy_network::{Ethereum, EthereumWallet, TransactionBuilder};
-use alloy_primitives::{Address, U256};
+use alloy_primitives::{Address, TxKind, U256};
 use alloy_primitives::bytes::BytesMut;
 use alloy_primitives::private::alloy_rlp::Encodable;
 use alloy_provider::fillers::{FillProvider, JoinFill, WalletFiller};
 use alloy_provider::{Identity, ReqwestProvider};
+use alloy_sol_types::sol;
 use antelope::api::v1::structs::{GetInfoResponse, GetTableRowsParams, IndexPosition, TableIndexType};
 use antelope::chain::action::{Action, PermissionLevel};
 use antelope::chain::checksum::{Checksum160, Checksum256};
@@ -14,14 +15,14 @@ use antelope::chain::Packer;
 use antelope::{name, StructPacker};
 use antelope::serializer::{Decoder, Encoder};
 use antelope::chain::private_key::PrivateKey;
-use alloy_rpc_types::TransactionRequest;
+use alloy_rpc_types::{TransactionInput, TransactionRequest};
 use alloy_signer_local::PrivateKeySigner;
 use alloy_transport_http::Http;
 use antelope::api::client::{APIClient, DefaultProvider};
 use antelope::chain::asset::{Asset, Symbol};
 use lazy_static::lazy_static;
 use reqwest::Client;
-use telos_translator_rs::types::evm_types::{AccountRow, EvmContractConfigRow};
+use telos_translator_rs::types::evm_types::{AccountRow, EvmContractConfigRow, SetRevisionAction};
 
 // reth provider type
 pub(crate) type TestProvider = FillProvider<JoinFill<Identity, WalletFiller<EthereumWallet>>, ReqwestProvider, Http<Client>, Ethereum>;
@@ -455,6 +456,139 @@ pub(crate) async fn doresources_sandwich(
         header: trx_header,
         context_free_actions: vec![],
         actions: vec![pre_tx, set_res_action, res_action, post_tx],
+        extension: vec![],
+    }
+}
+
+#[allow(dead_code)]
+pub(crate) async fn setrevision_sandwich(
+    info: &GetInfoResponse,
+    ram_payer: Name,
+    chain_id: u64,
+    start_nonce: u64,
+    gas_price: u128,
+    address: Address,
+) -> Transaction {
+    let trx_header = info.get_transaction_header(90);
+
+    sol! {
+        #[sol(rpc, bytecode="608060405234801561000f575f80fd5b505f805f555060ac806100215f395ff3fe6080604052348015600e575f80fd5b50600436106026575f3560e01c80636d619daa14602a575b5f80fd5b60306044565b604051603b9190605f565b60405180910390f35b5f5481565b5f819050919050565b6059816049565b82525050565b5f60208201905060705f8301846052565b9291505056fea26469706673582212205399ccd43cebd796d856269bea7d6b15156d118f97662b3f5e24089a290c6d6e64736f6c63430008160033")]
+        contract Push0Test {
+            uint256 public storedValue;
+        
+            constructor() {
+                assembly {
+                    // Use bytecode injection to insert PUSH0 manually
+                    let zero := 0
+                    sstore(storedValue.slot, zero)
+                }
+            }
+        }
+    }
+
+    let legacy_tx = alloy_consensus::TxLegacy {
+        chain_id: Some(chain_id),
+        nonce: start_nonce,
+        gas_price: gas_price,
+        gas_limit: 20_000_000,
+        to: TxKind::Create,
+        value: U256::ZERO,
+        input: Push0Test::BYTECODE.to_vec().into(),
+    };
+
+    let eth_pre_tx_typed = TransactionRequest {
+        from: Some(alloy_primitives::Address(*address)),
+        to: Some(legacy_tx.to),
+        gas: Some(legacy_tx.gas_limit as u64),
+        gas_price: Some(legacy_tx.gas_price),
+        value: Some(legacy_tx.value),
+        input: TransactionInput::from(legacy_tx.input.clone()),
+        nonce: Some(legacy_tx.nonce),
+        chain_id: legacy_tx.chain_id,
+        ..Default::default()
+    };
+
+    let eth_pre_tx_envelope = eth_pre_tx_typed.clone().build(&EOSIO_WALLET.clone()).await.unwrap();
+    let mut eth_pre_tx_raw_buf = BytesMut::new();
+    eth_pre_tx_envelope.encode(&mut eth_pre_tx_raw_buf);
+
+    let eth_pre_tx_raw: Vec<u8> = eth_pre_tx_raw_buf.to_vec();
+
+    // generate native transaction
+    #[derive(Clone, Eq, PartialEq, Default, StructPacker)]
+    struct Raw {
+        ram_payer: Name,
+        tx: Vec<u8>,
+        estimate_gas: bool,
+        sender: Option<Checksum160>,
+    }
+
+    #[derive(Clone, Eq, PartialEq, Default, StructPacker)]
+    struct SetResourcesParams {
+        gas_per_byte: u64,
+        target_free: u64,
+        min_buy: u64,
+        fee_transfer_pct: u64,
+    }
+
+    let pre_raw_data = Raw {
+        ram_payer,
+        tx: eth_pre_tx_raw,
+        estimate_gas: false,
+        sender: None,
+    };
+
+    let pre_tx = Action::new_ex(
+        name!("eosio.evm"),
+        name!("raw"),
+        vec![PermissionLevel::new(name!("eosio"), name!("active"))],
+        pre_raw_data,
+    );
+
+    let set_rev_action = Action::new(
+        name!("eosio.evm"),
+        name!("setrevision"),
+        PermissionLevel::new(name!("eosio.evm"), name!("active")),
+        SetRevisionAction {
+            new_revision: 0,
+        },
+    );
+
+    let eth_post_tx_typed = TransactionRequest {
+        from: Some(alloy_primitives::Address(*address)),
+        to: Some(legacy_tx.to),
+        gas: Some(legacy_tx.gas_limit as u64),
+        gas_price: Some(legacy_tx.gas_price),
+        value: Some(legacy_tx.value),
+        input: TransactionInput::from(legacy_tx.input.clone()),
+        nonce: Some(legacy_tx.nonce+1),
+        chain_id: legacy_tx.chain_id,
+        ..Default::default()
+    };
+    let eth_post_tx_envelope = eth_post_tx_typed.build(&EOSIO_WALLET.clone()).await.unwrap();
+    let mut eth_post_tx_raw_buf = BytesMut::new();
+    eth_post_tx_envelope.encode(&mut eth_post_tx_raw_buf);
+
+    let eth_post_tx_raw: Vec<u8> = eth_post_tx_raw_buf.to_vec();
+
+    let post_raw_data = Raw {
+        ram_payer,
+        tx: eth_post_tx_raw,
+        estimate_gas: false,
+        sender: None,
+    };
+
+    let post_tx = Action::new_ex(
+        name!("eosio.evm"),
+        name!("raw"),
+        vec![PermissionLevel::new(name!("eosio"), name!("active"))],
+        post_raw_data,
+    );
+
+    Transaction {
+        header: trx_header,
+        context_free_actions: vec![],
+        actions: vec![pre_tx, set_rev_action, post_tx],
         extension: vec![],
     }
 }
